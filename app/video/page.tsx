38820edc8d9671f1a04596ada1d07a033d86a4f1
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2024-09-02 00:09:50 -0300

    Revert "feat: add cloudinary config for uploads"
    This reverts commit 17e904f255d548db5c3bbc47462412de0ba8ddda.

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,26 +1,3 @@
-"use client";
-import { CldImage } from 'next-cloudinary';
-import { CldUploadButton } from 'next-cloudinary';
-
-// By default, the CldImage component applies auto-format and auto-quality to all delivery URLs for optimized delivery.
-export default function Page() {
-
-  return (
-    <>
-        <CldImage
-          src="cld-sample-5" // Use this sample image or upload your own via the Media Explorer
-          width="500" // Transform the image: auto-crop to square aspect_ratio
-          height="500"
-          crop={{
-              type: 'auto',
-              source: true
-          }} alt={''} />
-        <CldUploadButton uploadPreset="dejy4kq2" />
-
-        <video width="320" height="240" controls preload="none">
-            <source src="https://res.cloudinary.com/da0wykyjc/video/upload/v1724681793/pcf0nyjqsperi0rlck9f.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-    </>
-  );
+export default function Video() {
+    return <h1>página do vídeo</h1>
 }
